f0b38d78ab91043edf7b5c1a5d1cd510c7266daf
avasthiji <avasthi.d2itechnology@gmail.com>
2023-12-22 18:08:05 +0530

    article and blog page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
-
+body.classList.toggle('dark-mode');
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
