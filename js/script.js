4a8318bdb9dba95410a2194b553942daef94e375
avasthiji <avasthi.d2itechnology@gmail.com>
2023-12-22 18:10:26 +0530

    default dark

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
-body.classList.toggle('dark-mode');
+body.classList.add('dark-mode');
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
