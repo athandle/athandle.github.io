e67b26a0ebd55d0b24ec996caef5cd56bad9ca07
avasthiji <avasthi.d2itechnology@gmail.com>
2024-01-05 00:38:26 +0530

    mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const themeToggle = document.getElementById('themeToggle');
+const themeToggleWeb = document.getElementById('themeToggleWeb');
+
 const body = document.body;
 body.classList.add('dark-mode');
 themeToggle.addEventListener('click', () => {
+    console.log("5")
+    body.classList.toggle('dark-mode');
+});
+themeToggleWeb.addEventListener('click', () => {
+    console.log("5")
     body.classList.toggle('dark-mode');
 });
